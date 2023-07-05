5b3d494bf5adbf1da03ad8b466dffcd77e57d01b
RAJ KUMAR SEN <rajkumarshrivash374@gmail.com>
2023-07-05 13:43:23 +0530

    terminal based game created

--- app/main.py
+++ app/main.py
@@ -1,11 +1,32 @@
+from flask import Flask, render_template, request, jsonify
+
 from Controller.gameController import GameController
 
-if __name__ == "__main__":
-    controller = GameController()
+# if __name__ == "__main__":
+#     controller = GameController()
     
-    while True:
-        controller.start()
+#     while True:
+#         controller.start()
         
-        play_again = input("Do you want to play again? (y/n): ").lower()
-        if play_again != 'y':
-            break
+#         play_again = input("Do you want to play again? (y/n): ").lower()
+#         if play_again != 'y':
+#             break
+
+
+app = Flask(__name__)
+controller = GameController()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/play', methods=['POST'])
+def play():
+    choice = request.json['choice']
+    controller.model.set_player_choice(choice)
+    controller.start()
+    result = controller.view.model.result
+    return jsonify({'result': result})
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ app/static/js/script.js
@@ -0,0 +1,19 @@
+function playGame(choice) {
+    fetch('/play', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ choice: choice })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('result').innerText = data.result;
+        document.getElementById('play-again-btn').style.display = 'block';
+    });
+}
+
+function playAgain() {
+    document.getElementById('result').innerText = '';
+    document.getElementById('play-again-btn').style.display = 'none';
+}
